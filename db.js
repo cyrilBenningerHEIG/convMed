5c79bf86b46a9d335811fb98ea5a328585526845
loic.postec <loic.postec@heig-vd.ch>
2019-11-20 14:56:39 +0100

    fix db url

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function startDb() {
-  mongoose.connect('mongodb://localhost:27017/quizz', {
+  mongoose.connect('mongodb://admin:Admin123@ds047958.mlab.com:47958/heroku_vcpp2jlx', {
     useNewUrlParser: true,
   })
   .then(() => console.log('Mongodb successully connected'))
