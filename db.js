1401d221e3609bd45cd7756b9adc705982d0b481
loic.postec <loic.postec@heig-vd.ch>
2019-11-20 14:17:48 +0100

    fix db.js

--- db.js
+++ db.js
@@ -3,13 +3,9 @@ const mongoose = require('mongoose');
 function startDb() {
   mongoose.connect('mongodb://localhost:27017/quizz', {
     useNewUrlParser: true,
-    keepAlive: 1
   })
-  .then(() => console.log('Mongodb successully connected'));
-
-  return mongoose.connection
-    .on('error', console.error)
-    .on('disconnected', startDb);
+  .then(() => console.log('Mongodb successully connected'))
+  .catch(err => console.log(err));
 }
 
 function closeDb() {
