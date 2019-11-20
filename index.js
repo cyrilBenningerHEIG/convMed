b40b5d711e1635213473642624180aa859549e70
loic.postec <loic.postec@heig-vd.ch>
2019-11-20 13:35:14 +0100

    refix port 8000

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res) => {
 
 startDb()
   .once('open', () => {
-    app.listen(3000, () => {
-      console.log('App is listening on port 3000');
+    app.listen(8000, () => {
+      console.log('App is listening on port 8000');
     });
   });
