8383483766109c4919914fc0f3e9e5192b200870
loic.postec <loic.postec@heig-vd.ch>
2019-11-20 15:02:31 +0100

    try to fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var app = express();
-import db from "db.js";
+var db = require("db.js")
 
 
 //
