e3883dd2ad54d276c06c0ef44cbc0dc9f562450b
loic.postec <loic.postec@heig-vd.ch>
2019-11-20 13:58:40 +0100

    add quiz schema and quiz route

---
+++ models/quiz.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const quizSchema = new Schema({
+    question_txt : String, 
+    question_img : String,
+    rep_juste : String,
+    rep_fausse1 : String, 
+    rep_fausse2 : String, 
+    rep_fausse3 : String, 
+    info : String, 
+    info2 : String, 
+    type : Boolean
+    });
+
+module.exports = mongoose.model('Quiz', quizSchema); 
