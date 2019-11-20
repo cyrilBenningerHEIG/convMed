50ac9158c4347379f21e2f826a2b821c64f612f6
loic.postec <loic.postec@heig-vd.ch>
2019-11-20 12:31:54 +0100

    rm test jade

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var app = express();
-
-////////
-app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'KJSDHAKJSH' }
-  )
-})
 
 
 //
