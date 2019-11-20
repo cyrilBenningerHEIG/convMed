a8e22f96bbaa5a9657df24beb1929fd94b39f28f
Gaëtan Haefeli <gagah98@gmail.com>
2019-11-20 12:02:24 +0100

    add datas

---
+++ data.js
@@ -0,0 +1,285 @@
+const data = [
+    {
+        "questiontxt": "Vous ne parlez que d'aller de l'avant avec les mêmes mauvaises idées qui nous ont mis dans ce pétrin, même si la seule chose raisonnable à faire est de tirer le frein à main.",
+        "repjuste": "Greta Thunberg\n",
+        "repfausse1": "Lewis Hamilton",
+        "repfausse2": "Nicolas Hulot",
+        "repfausse3": "Darius Rochebin",
+        "repphoto": "img/quote/greta",
+        "info2": 2018,
+        "info": "C'est une citation tirée du discours percutant de Greta Thunberg qui fait face aux plus grands hommes politiques lors d'un sommet de l'ONU, où elle dénonce les inactions des dirigeants.  ",
+        "type": 0
+    },
+    {
+        "questiontxt": "L'européen oublie trop souvent qu'il n'est rien face à la nature qui reprend ses droits ",
+        "repjuste": "Nekfeu",
+        "repfausse1": "Jean-Jacques Rousseau ",
+        "repfausse2": "Lao Tseu",
+        "repfausse3": "Greta Thunberg\n",
+        "repphoto": "img/quote/nekfeu",
+        "info2": 2019,
+        "info": "C'est un titre du rappeur français Nekfeu, Premier pas, tiré de son dernière album Les Étoiles Vagabondes. Comme quoi, malgré leur réputation, les rappeurs peuvent être touchés par des causes primordiales comme l'écologie. ",
+        "type": 0
+    },
+    {
+        "questiontxt": "Pour qu'un écologiste soit élu président, il faudrait que les arbres votent.",
+        "repjuste": "Coluche",
+        "repfausse1": "Groot",
+        "repfausse2": "Bob Marley",
+        "repfausse3": "Olivier Français",
+        "repphoto": "img/quote/coluche",
+        "info": "C'est Coluche ! A son époque, il était déjà conscient de l'enjeu de l'écologie.",
+        "type": 0
+    },
+    {
+        "questiontxt": " Si les membres de l’ONU savaient ce que je ressens vraiment... À quel point je suis pessimiste face à l’avenir... Peut-être qu’ils se diraient qu’ils ont choisi le mauvais homme. ",
+        "repjuste": "Leonardo DiCaprio",
+        "repfausse1": "Jean-Claude Juncker",
+        "repfausse2": "Gandhi",
+        "repfausse3": "Boris Johnson",
+        "repphoto": "img/quote/leo",
+        "info2": 2016,
+        "info": "Cette citation est tirée du film Before The Flood. Il a été réalisé par DiCaprio qui est un activiste pour l'écologie (2016)",
+        "type": 0
+    },
+    {
+        "questiontxt": "Si l'on m'apprenait que la fin du monde est pour demain, je planterais quand même un pommier.",
+        "repjuste": "Martin Luther",
+        "repfausse1": "Yannick Noah",
+        "repfausse2": "Jacques Prévert",
+        "repfausse3": "Isaac Newton",
+        "repphoto": "img/quote/mlk",
+        "info2": "début XVIe siècle",
+        "info": "C'est Martin Luther, le philosophe (et non pas Martin Luther King), qui a écrit cette phrase pleine d'espoir et de poésie.",
+        "type": 0
+    },
+    {
+        "questiontxt": "C'est une triste chose de songer que la nature parle et que le genre humain ne l'écoute pas.",
+        "repjuste": "Victor Hugo",
+        "repfausse1": "Dalaï Lama",
+        "repfausse2": "Adèle Thorens",
+        "repfausse3": "Mike Horn",
+        "repphoto": "img/quote/hugo",
+        "info2": "XIXe siècle",
+        "type": 0
+    },
+    {
+        "questiontxt": "Le réchauffement climatique, c'est dû à la chatte à ta mère",
+        "repjuste": "Booba",
+        "repfausse1": "Donald Trump",
+        "repfausse2": "Jean-Marie Bigard",
+        "repfausse3": "Kanye West",
+        "repphoto": "img/quote/booba",
+        "info2": 2018,
+        "info": "Booba, le rappeur impuslif et toujours en finesse, écrit cette phrase dans un couplet de son morceau BB",
+        "type": 0
+    },
+    {
+        "questiontxt": "Nous avons une seule et unique mission: Protéger la planète et passer le relai à la prochaine génération",
+        "repjuste": "François Hollande",
+        "repfausse1": "Grand Corps Malade",
+        "repfausse2": "Alain Morisod",
+        "repfausse3": "Angela Merkel",
+        "repphoto": "img/quote/holland",
+        "info": "François Hollande exprime ces mots en 2015, lors d'un discour au World Economic Forum de Davos.",
+        "type": 0
+    },
+    {
+        "questiontxt": " Le capitalisme ne fait pas que épuiser le travailleur, il épuise aussi la terre",
+        "repjuste": "Karl Marx",
+        "repfausse1": "Kery James",
+        "repfausse2": "Bernand Henry Levy",
+        "repfausse3": "Che Guevara",
+        "repphoto": "img/quote/karlmarx",
+        "info2": 1867,
+        "info": "Cette phrase sort tout droit du livre Capital de Karl Marx ",
+        "type": 0
+    },
+    {
+        "questiontxt": "Pendant que je recherche mon jardin, les hommes effacent leurs forêts",
+        "repjuste": "Alpha Wann",
+        "repfausse1": "Gandhi",
+        "repfausse2": "Barack Obama",
+        "repfausse3": "Jair Bolsonaro",
+        "repphoto": "img/quote/phillyflingue",
+        "info2": 2015,
+        "info": "C'est un extrait du morceau Playoffs, écrite par le rappeur parisien Alpha Wann",
+        "type": 0
+    },
+    {
+        "questiontxt": "Je ne pense pas que ce soit un canular. Mais je ne sais pas si l'action humaine est responsable. Et ça pourra revenir en arrière",
+        "repjuste": "Donald Trump",
+        "repfausse1": "Emmanuel Macron",
+        "repfausse2": "Vincent Bolloré",
+        "repfausse3": "Mark Zuckerberg",
+        "repphoto": "img/quote/pute",
+        "info2": 2018,
+        "info": "Donald Trump, président des USA, a dit cette phrase lors d'un entretien 60 minutes de CBS",
+        "type": 0
+    },
+    {
+        "questiontxt": "Les espèces qui survivent ne sont pas les espèces les plus fortes, ni les plus intelligentes, mais celles qui s'adaptent le mieux aux changements",
+        "repjuste": "Charles Darwin",
+        "repfausse1": "Arnold Schwarzenegger",
+        "repfausse2": "Optimus Prime",
+        "repfausse3": "Bill Gates",
+        "repphoto": "img/quote/darwin",
+        "info": "C'est une phrase dite par Charles Darwin, naturaliste et paléontologue, qui accompagne bien sa théorie de sélection naturelle",
+        "type": 0
+    },
+    {
+        "questiontxt": "Nous n'héritons pas de la terre de nos parents, nous l'empruntons à nos enfants.",
+        "repjuste": "Antoine de Saint-Exupéry",
+        "repfausse1": "Orelsan",
+        "repfausse2": "L'abbé Pierre",
+        "repfausse3": "Henri Dès",
+        "repphoto": "img/quote/adse",
+        "info2": "début XXe siècle",
+        "info": "Antoine de Saint Exupéry, écrivain et aviateur Français du 20ème siècle, a dit ces mots. Il est notamment connu pour son ouvrage Le Petit Prince",
+        "type": 0
+    },
+    {
+        "questiontxt": "Quand le dernier arbre aura été abattu, quand la dernière rivière aura été empoisonnée , quand le dernier poisson aura été péché, alors on saura que l'argent ne se mange pas.",
+        "repjuste": "Geronimo",
+        "repfausse1": "Philippe Etchebest",
+        "repfausse2": "Bear Grylls",
+        "repfausse3": "Justin Bridou",
+        "repphoto": "img/quote/geronimo",
+        "info2": "XIXe siècle",
+        "type": 0
+    },
+    {
+        "questiontxt": "Notre maison brûle et nous regardons ailleurs.",
+        "repjuste": "Jacques Chirac",
+        "repfausse1": "Alain Souchon",
+        "repfausse2": "Barack Obama",
+        "repfausse3": "Yann Marguet",
+        "repphoto": "img/quote/chirac",
+        "info2": 2002,
+        "type": 0
+    },
+    {
+        "questiontxt": "Contrairement aux chasseurs qui, eux, ne sont pas des lapins, les pollueurs, eux, sont des ordures.",
+        "repjuste": "Philippe Geluck",
+        "repfausse1": "Renaud",
+        "repfausse2": "Jean-Luc Mélenchon",
+        "repfausse3": "Coluche",
+        "repphoto": "img/quote/geluck",
+        "info2": 2006,
+        "type": 0
+    },
+    {
+        "questiontxt": "C'est quand le puit est à sec que nous connaissons la valeur de l'eau.",
+        "repjuste": "Benjamin Franklin",
+        "repfausse1": "Daenerys Targaryen",
+        "repfausse2": "Mike Horn",
+        "repfausse3": "Geronimo",
+        "repphoto": "img/quote/franklin",
+        "info2": "XVIIIe siècle",
+        "type": 0
+    },
+    {
+        "questionimg": "img/meat/quest/poulet",
+        "repjuste": "Poulet",
+        "repfausse1": "Canard",
+        "repfausse2": "Boeuf",
+        "repfausse3": "Porc",
+        "repphoto": "img/meat/rep/poulet",
+        "info2": "Rapport Agricole 2019",
+        "info": "14kg / personne / an",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/pigeon",
+        "repjuste": "Pigeon",
+        "repfausse1": "Canard",
+        "repfausse2": "Boeuf",
+        "repfausse3": "Rat",
+        "repphoto": "img/meat/rep/pigeon",
+        "info": "On ne consomme que les pigeons d’élevage, pas ceux des villes",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/boeuf",
+        "repjuste": "Boeuf",
+        "repfausse1": "Rat",
+        "repfausse2": "Lièvre",
+        "repfausse3": "Poulet",
+        "repphoto": "img/meat/rep/boeuf",
+        "info2": "Rapport Agricole 2019",
+        "info": "11kg / personne / an",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/agneau",
+        "repjuste": "Agneau",
+        "repfausse1": "Serpent",
+        "repfausse2": "Autruche",
+        "repfausse3": "Cheval",
+        "repphoto": "img/meat/rep/agneau",
+        "info": "En Europe, on consomme plutôt du mouton, et de l’agneau aux USA",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/lapin",
+        "repjuste": "Lapin",
+        "repfausse1": "Poulet",
+        "repfausse2": "Grenouille",
+        "repfausse3": "Chat",
+        "repphoto": "img/meat/rep/lapin",
+        "info": "En Suisse, on consomme 50kg de viande par année l’équivalent de 40 lapins nains",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/cochondinde",
+        "repjuste": "Cochon d'Inde",
+        "repfausse1": "Poulet",
+        "repfausse2": "Lion",
+        "repfausse3": "Grenouille",
+        "repphoto": "img/meat/rep/cochondinde",
+        "info": "Dicton inca :  Elève des cochons d’Inde et mange à ta faim ",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/chien",
+        "repjuste": "Chien",
+        "repfausse1": "Boeuf",
+        "repfausse2": "Humain",
+        "repfausse3": "Serpent",
+        "repphoto": "img/meat/rep/chien",
+        "info": "En Suisse, il est légal de consommer de la viande de chien",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/chat",
+        "repjuste": "Chat",
+        "repfausse1": "Chien",
+        "repfausse2": "Lapin",
+        "repfausse3": "Cheval",
+        "repphoto": "img/meat/rep/chat",
+        "info": "3% des Suisses consommeraient discrètement du chat et du chien",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/plantes",
+        "repjuste": "Plantes",
+        "repfausse1": "Boeuf",
+        "repfausse2": "Alligator",
+        "repfausse3": "Porc",
+        "repphoto": "img/meat/rep/plantes",
+        "info": "Noix de coco, colza, pois, pomme de terre et bambou",
+        "type": 1
+    },
+    {
+        "questionimg": "img/meat/quest/champignon",
+        "repjuste": "Champignon",
+        "repfausse1": "Lapin",
+        "repfausse2": "Poulet",
+        "repfausse3": "Chat",
+        "repphoto": "img/meat/rep/champignon",
+        "info": "Champignons avec du brocoli et du tofu",
+        "type": 1
+    }
+]
+
+module.exports = {data}
